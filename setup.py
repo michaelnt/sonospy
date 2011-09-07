1b37390e811ed5bec84b76dcf7ba2129e73e389c
Michael Thompson <michaelnt@gmail.com>
2011-09-07 13:40:39 +0100

    Add a setup.py so that python setup.py develop puts the current directory onto the python path

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "sonospy",
+    description = "UPNP Controller for Sonos renderer",
+    version = "",
+    author = "",
+    packages = ["sonospy"],
+    #package_dir = {'':"./"},
+    )
